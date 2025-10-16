aace0bf3a0557b1160f7da223914334e0c7259df
Vicientt <nguyen15@kenyon.edu>
2025-10-16 14:33:17 -0400

    Part 5

--- src/balance.cpp
+++ src/balance.cpp
@@ -8,7 +8,24 @@ static bool is_balanced_rec(const std::string& s, int idx, int open) {
     // - If '(' => recurse open+1
     // - If ')' => recurse open-1
     // - else ignore
-    return false;
+    if (open < 0) {
+        return false;
+    }
+
+
+    if (idx == s.size()) {
+        return open == 0;
+    }
+
+    char current_char = s[idx];
+
+    if (current_char == '(') {
+        return is_balanced_rec(s, idx + 1, open + 1);
+    } else if (current_char == ')') {
+        return is_balanced_rec(s, idx + 1, open - 1);
+    } else {
+        return is_balanced_rec(s, idx + 1, open);
+    }
 }
 
 bool is_balanced(const std::string& s) {
